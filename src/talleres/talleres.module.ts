f2c99f860cef9ac16ad2351a3e0a5e9e7d0d0e50
Luis Neyra <luisnm93@gmail.com>
2025-11-06 17:58:38 -0600

    [ADD] aguante talleres

---
+++ src/talleres/talleres.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TalleresService } from './talleres.service';
+import { TalleresController } from './talleres.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Talleres } from 'src/entities/Talleres';
+import { BitacoraModule } from 'src/bitacora/bitacora.module';
+import { Clientes } from 'src/entities/Clientes';
+
+@Module({
+  imports:[TypeOrmModule.forFeature([Talleres,Clientes]),BitacoraModule],
+  controllers: [TalleresController],
+  providers: [TalleresService],
+})
+export class TalleresModule {}
